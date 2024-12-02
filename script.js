392561361773e1077a81df81e51be1dc66383345
Cats1337 <42129397+Cats1337@users.noreply.github.com>
2024-12-02 00:28:59 -0500

    Save

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    const checkbox = document.getElementById("light");
+    if (checkbox.checked) {
+        darkMode();
+    }
+};
+
+function darkMode() {
+    let element = document.body;
+    element.classList.toggle("dark");
+    // transition: background-color 0.5s ease;
+    element.style.transition = "background-color 0.5s ease";
+    element.style.transition = "color 0.5s ease";
+}
+
+
+  
